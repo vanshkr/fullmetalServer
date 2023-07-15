54599e62c0936de39075d3e362f16c7966268157
vanshkr <krvansh30@gmail.com>
2023-07-15 22:33:22 +0530

    loginSchema

--- controllers/users.js
+++ controllers/users.js
@@ -1,9 +1,10 @@
 import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
+import dotenv from 'dotenv';
 
 import UserModal from "../models/userSchema.js";
 
-const secret = 'test';
+// const secret = 'test';
 
 export const logIn = async (req, res) => {
   const { email, password } = req.body;
@@ -16,7 +17,7 @@ export const logIn = async (req, res) => {
 
     if (!isPasswordCorrect) return res.status(400).json({ message: "Invalid credentials" });
 
-    const token = jwt.sign({ email: existingUser.email, id: existingUser._id }, secret, { expiresIn: "1h" });
+    const token = jwt.sign({ email: existingUser.email, id: existingUser._id },process.env.TOKEN_KEY, { expiresIn: "1h" });
     
     res.status(200).json({ result: existingUser, token });
   } catch (err) {
@@ -31,12 +32,12 @@ export const register = async (req, res) => {
     const existingUser = await UserModal.findOne({ email });
     if (existingUser) return res.status(400).json({ message: "User already exists" });
 
-    if(password !== confirmPassword) return res.status(400).json({ message: "User already exists" });
+    if(password !== confirmPassword) return res.status(400).json({ message: "Passwords do not match" });
     const hashedPassword = await bcrypt.hash(password, 12);
 
     const result = await UserModal.create({ email, password: hashedPassword, name: `${fullName}` });
-
-    const token = jwt.sign( { email: result.email, id: result._id }, secret, { expiresIn: "1h" } );
+    
+    const token = jwt.sign( { email: result.email, id: result._id },process.env.TOKEN_KEY, { expiresIn: "1hr" } );
     res.status(201).json({ result, token });
     
   } catch (error) {
--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.use(cors());
 app.use('/user',userRoutes);
 
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 
 mongoose.connect(process.env.CONNECTION_URL)
