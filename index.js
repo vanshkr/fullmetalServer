b91704b08e1608b166e298e21348ecb723d1e6e4
vanshkr <krvansh30@gmail.com>
2023-08-08 22:56:46 +0530

    added watchlist router

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
-import express from 'express';
+
+
+  import express from 'express';
 import mongoose from 'mongoose';
 import bodyParser from 'body-parser';
 import cors from 'cors';
 import dotenv from 'dotenv';
 import userRoutes from './routes/users.js';
+import watchlistRoutes from './routes/watchlist.js';
+
 
 const app = express();
 dotenv.config();
@@ -12,6 +16,7 @@ app.use(bodyParser.json({limit : "30mb",extended:true}));
 app.use(bodyParser.urlencoded({limit : "30mb",extended:true}));
 app.use(cors());
 app.use('/user',userRoutes);
+app.use('/watchlist',watchlistRoutes);
 
 
 const PORT = process.env.PORT ;
